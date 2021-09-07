f27601da101b0043babcc3c3cc38e3aa0e33027e
cnunez6310 <89819678+cnunez6310@users.noreply.github.com>
2021-09-06 22:46:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
+function Computeint()
 {
-    p = document.getElementById("principal").value;
-    
+  //Principal validation
+        if(amt.value == "")
+         {
+          alert("Amount can't be 0, please enter an amount");
+          amt.focus();
+          return false;
+         }
+//grabing today's year and adding amount of years
+         {
+           year3 = (years * 1);
+           var d = new Date();
+           var n = d.getFullYear();
+           var n1 = parseInt(years);
+           var year2 =  (years*1)+n1;
+         }
+    amt = document.getElementById("amt").value;
+    years = document.getElementById("years").value;
+    rate = document.getElementById("rate").value;
+    x1 = document.getElementById("x1");
+    cantidad=document.getElementById("cantidad");
+    total = document.getElementById("total");
+    porcentaje = document.getElementById("porcentaje");
+    cantidad.innerHTML="If you deposit " +amt;
+    porcentaje.innerHTML="at an interest rate of " +rate +"%";
+    total.innerHTML="You will receive an amount of   " + (amt*years*rate/100);
+    x1.innerHTML="in the year " +year3;
 }
         
